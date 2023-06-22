860f4b10a8a55d601b0bfbfbdae6471a5feb6738
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-22 08:05:06 +0300

    new dumpfile

--- spring_db_dump.sql
+++ spring_db_dump.sql
@@ -44,32 +44,6 @@ INSERT INTO `book` VALUES (1,'JS Programming','Jim Jones','123-456-77'),(2,'PHP 
 UNLOCK TABLES;
 
 --
--- Table structure for table `book_table`
---
-
-DROP TABLE IF EXISTS `book_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book_table` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `author` varchar(255) NOT NULL,
-  `isbn` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `book_table`
---
-
-LOCK TABLES `book_table` WRITE;
-/*!40000 ALTER TABLE `book_table` DISABLE KEYS */;
-INSERT INTO `book_table` VALUES (1,'Teppo Testi','123-456','Spring Boot');
-/*!40000 ALTER TABLE `book_table` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -94,31 +68,6 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'user01','$2a$10$v7abPP4OrLbTRlw6TmnWOul94dCyoQ7s6BnFzMW1.P.Z621lRQ8ui');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user_table`
---
-
-DROP TABLE IF EXISTS `user_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_table` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_table`
---
-
-LOCK TABLES `user_table` WRITE;
-/*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
-INSERT INTO `user_table` VALUES (1,'$2a$10$tyjdVge4Yhsh19fu/tvhZeLk9L/.Ndg.PYeu1A94rPYUGnQxFXPvK','user01');
-/*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -129,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-22  7:12:14
+-- Dump completed on 2023-06-22  8:04:44
